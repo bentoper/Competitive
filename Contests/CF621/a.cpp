cdcd1f9e3a426063155dd0159bfe580b629743c9
bentopereira <bento.pereira@usp.br>
2020-02-20 14:17:55 -0300

    small update

---
+++ Contests/CF621/a.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+
+#define fr(i,n) 	for(int i=0;i<n;i++)
+#define frr(i,n)	for(int i=1;i<=n;i++)
+#define pv(x, n)    fr(iii, n) printf("%d%c", x[iii], " \n"[iii==n-1])
+#define pvv(x, n)    frr(iii, n) printf("%d%c", x[iii], " \n"[iii==n])
+#define vp(v)        pv(v, v.size()) 
+
+
+#define ms(x,i)	memset(x,i,sizeof(x))
+#define dbg(x)	cout << #x << " = " << x << endl
+#define all(x)	x.begin(),x.end()
+#define otp(x) cout << x << endl;
+#define rvr(x) int x; scanf("%d", &x)
+#define gnl cout << endl
+#define olar cout << "olar" << endl
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef vector<ll> vll;
+typedef pair<ll,ll> pll;
+
+const int INF = 0x3f3f3f3f;
+const ll llINF = 0x3f3f3f3f3f3f3f;
+
+int main(){
+    rvr(t);
+    while(t--){
+        int n, d;
+        scanf("%d%d", &n, &d);
+        vi pi;
+        fr(i, n){
+            rvr(a);
+            pi.pb(a);
+        }
+        int st = 0, i = 1;
+        while(pi.size() > 1 && d > 0 && i < n){
+            if(pi[i] > 0){
+                pi[0] += min((d/i), pi[i]);
+                d -= min((d/i), pi[i])*i;
+            }
+            i++;
+        }
+        printf("%d\n", pi[0]);
+    }
+}

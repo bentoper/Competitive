0b4d7edc9c0c1d29ad892ef1e674913446be61ce
bentopereira <bento.pereira@usp.br>
2020-03-09 18:32:11 -0300

    small updt

--- NT/calcada.cpp
+++ NT/calcada.cpp
@@ -28,30 +28,16 @@ typedef pair<ll,ll> pll;
 const int INF = 0x3f3f3f3f;
 const ll llINF = 0x3f3f3f3f3f3f3f;
 
-int n, a, b, x, y;
-
-int gcd(int a, int b){
-	return a%b==0?b:gcd(b, a%b);
-}
+int n, a, b, c, d;
 
 int main(){
 	scanf("%d",&n);
 	fr(i, n){
-		scanf("%d%d%d%d", &a, &b, &x, &y);
-		if(x - a == 0) printf("%d", abs(b-y)+1);
-		else if(y - b == 0) printf("%d", abs(x-a)+1);
-		else{
-			int d = gcd(abs(x-a), abs(y-b));
-			if(x - a < 0 && y - b < 0){
-				int aux1 = a, aux2 = b;
-				a = x;
-				b = y;
-				x = aux1;
-				b = aux2;
-				int dy = (x - a)/d;
-				int resp;
-				if(x - a != d) resp = (y - b)/dy;
-			}
+		scanf("%d%d%d%d", &a, &b, &c, &d);
+		if(a - c == 0 || b - d == 0){
+			printf("%d\n", max(abs(a-c), abs(b-d)));
+			continue;
 		}
+		printf("%d\n", __gcd(abs(a-c), abs(d-b)) + 1);
 	}
 }

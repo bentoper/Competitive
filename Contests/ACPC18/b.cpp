1a0ae7a64f50eb14420823b87115f9b2be11b3e3
bentopereira <bento.pereira@usp.br>
2020-06-29 16:54:56 -0300

    group contest

--- Contests/ACPC18/b.cpp
+++ Contests/ACPC18/b.cpp
@@ -5,9 +5,15 @@ using namespace std;
 #define mp make_pair
 #define fst first
 #define snd second
+# define M_PI		3.14159265358979323846	/* pi */
+
 
 #define fr(i,n) 	for(int i=0;i<n;i++)
 #define frr(i,n)	for(int i=1;i<=n;i++)
+#define pv(x, n)    fr(iii, n) printf("%d%c", x[iii], " \n"[iii==n-1])
+#define pvv(x, n)    frr(iii, n) printf("%d%c", x[iii], " \n"[iii==n])
+#define vp(v)        pv(v, v.size()) 
+
 
 #define ms(x,i)	memset(x,i,sizeof(x))
 #define dbg(x)	cout << #x << " = " << x << endl
@@ -29,5 +35,16 @@ const int INF = 0x3f3f3f3f;
 const ll llINF = 0x3f3f3f3f3f3f3f;
 
 int main(){
+#ifdef ONLINE_JUDGE
+    freopen("baklava.in", "r", stdin);
+#endif
+    rvr(t);
+    while(t--){
+        double n; scanf("%lf", &n);
+        double ss = sin(((n - 2.0)*M_PI)/(2.0*n));
+        double otp = 1.0/(1.0 - (ss * ss));
+        double ans = 1e4 * otp;
+        printf("%.5lf\n", ans);
+    }
 
 }
